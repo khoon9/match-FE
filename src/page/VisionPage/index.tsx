0536fab4a5518edc8edcc992927d1519b93c39a7
khoon9 <kshun08@dongguk.edu>
2023-12-05 21:01:02 +0900

    feat: vision 페이지 골격 추가

--- src/page/VisionPage/index.tsx
+++ src/page/VisionPage/index.tsx
@@ -1,7 +1,20 @@
+import { IonButton, IonContent, IonHeader, IonPage } from "@ionic/react";
 import React from "react";
+import styled from "styled-components";
 
 const VisionPage = () => {
-  return <div>index</div>;
+  return (
+    <IonPage>
+      <IonHeader></IonHeader>
+      <IonContent>
+        <StyledApplyButton size="large">신청하기</StyledApplyButton>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default VisionPage;
+
+const StyledApplyButton = styled(IonButton)`
+  --border-radius: 1rem;
+`;
