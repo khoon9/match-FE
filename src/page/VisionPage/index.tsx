0b651f2d5d084c94d9e77decea0a05769bc5d4f1
khoon9 <kshun08@dongguk.edu>
2023-12-13 00:38:29 +0900

    feat: #3 방문자 및 조회 관리 적용
    - getUuid
    - postNewClientVisitor
    - updateVisitCount
    api 연동

--- src/page/VisionPage/index.tsx
+++ src/page/VisionPage/index.tsx
@@ -1,9 +1,61 @@
-import { IonButton, IonContent, IonHeader, IonPage } from "@ionic/react";
-import React, { useRef } from "react";
+import { IonButton, IonPage } from "@ionic/react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import SubmitModar from "../../components/SubmitModar";
+import storage from "../../utils/storage";
+import { customAxios } from "../../lib/customAxios";
+import { Loading } from "../../assets/loading/Loading";
 
 const VisionPage = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [uuid, setUuid] = useState<string>(storage.get("clientVisitor").uuid);
+  useEffect(() => {
+    if (window.location.pathname === "/vision") {
+      if (uuid !== "") {
+        setTimeout(() => setIsLoading(false), 500);
+        updateVisitCount(storage.get("clientVisitor").uuid);
+      } else if (uuid === "") {
+        getUuidfromServer();
+      }
+    }
+  }, [window.location.pathname]);
+
+  const getUuidfromServer = async () => {
+    await customAxios
+      .get("/")
+      .then((res) => {
+        storage.set("clientVisitor", { uuid: res.data });
+        setUuid(res.data);
+        postNewClientVisitor(res.data);
+        setTimeout(() => setIsLoading(false), 500);
+      })
+      .catch((e) => {
+        console.log(e);
+        setTimeout(() => alert("인터넷 연결을 확인해주세요."), 500);
+      });
+  };
+
+  const postNewClientVisitor = async (requestUuid: string) => {
+    await customAxios
+      .post("/", { uuid: requestUuid })
+      .then((_) => {
+        updateVisitCount(requestUuid);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const updateVisitCount = async (requestUuid: string) => {
+    await customAxios
+      .put("/visit", { uuid: requestUuid })
+      .then()
+      .catch((e) => {
+        console.log(e);
+        alert("조회 증가 실패");
+      });
+  };
+
   const submitModalRef = useRef<HTMLIonModalElement>(null);
 
   const handleSubmit = () => {
@@ -14,12 +66,19 @@ const VisionPage = () => {
   };
   return (
     <IonPage>
-      <BaseDiv>
-        <StyledTextDiv>본문 예시</StyledTextDiv>
-        <StyledApplyButton id="submitModarOpen" size="large">
-          신청하기
-        </StyledApplyButton>
-      </BaseDiv>
+      {isLoading ? (
+        <BaseDiv>
+          <Loading />
+        </BaseDiv>
+      ) : (
+        <BaseDiv>
+          {" "}
+          <StyledTextDiv>본문 예시</StyledTextDiv>
+          <StyledApplyButton id="submitModarOpen" size="large">
+            신청하기
+          </StyledApplyButton>
+        </BaseDiv>
+      )}
       <SubmitModar
         modal={submitModalRef}
         openId={"submitModarOpen"}
