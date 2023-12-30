1f4a687f550bb2aa261c5901758cd7ae8c9b097a
khoon9 <kshun08@dongguk.edu>
2023-12-30 16:48:34 +0900

    feat: 모달창 제출 형식 구현

--- src/components/Vision/SubmitModar.tsx
+++ src/components/Vision/SubmitModar.tsx
@@ -1,4 +1,14 @@
-import { IonButton, IonInput, IonModal } from "@ionic/react";
+import {
+  IonButton,
+  IonInput,
+  IonItem,
+  IonList,
+  IonModal,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+  IonTextarea,
+} from "@ionic/react";
 import React, { RefObject, useState } from "react";
 import styled, { css, keyframes } from "styled-components";
 
@@ -18,6 +28,9 @@ const SubmitModar = ({
   const [email, setEmail] = useState<string>("");
   const [isEmail, setIsEmail] = useState<boolean>(true);
   const [shouldShake, setShouldShake] = useState<boolean>(false);
+
+  const [content02, setContent02] = useState("");
+  const [content03, setContent03] = useState("");
 
   const initState = () => {
     setEmail("");
@@ -46,46 +59,98 @@ const SubmitModar = ({
   return (
     <StyledIonModal ref={modal} trigger={openId}>
       <BaseDiv>
-        <form
+        <FormTextDiv
           onSubmit={(e) => {
             handleCheck(e);
           }}
         >
-          <FormTextDiv>
-            시범운영 준비중입니다.
-            <br /> 이메일로 오픈 알람을 받으시겠습니까 ?{" "}
-          </FormTextDiv>
-          <StyledIonInputBox $shouldShake={shouldShake}>
-            <StyledIonInput
-              type="text"
-              placeholder="이메일을 입력해주세요"
-              value={email}
-              onIonInput={(e) => {
-                setEmail(e.detail.value as string);
-                if (!isEmail && checkIsEmail(email)) {
-                  setIsEmail(true);
-                }
-              }}
-            ></StyledIonInput>
-          </StyledIonInputBox>
+          <NormalInputContainerDiv>
+            <NormalInputTitleDiv>성별</NormalInputTitleDiv>
+            <NormalInputContentDiv
+              interface="popover"
+              placeholder="성별을 선택해주세요(선택)"
+            >
+              <NormalCategoryInput value="여성">여성</NormalCategoryInput>
+              <NormalCategoryInput value="남성">남성</NormalCategoryInput>
+            </NormalInputContentDiv>
+          </NormalInputContainerDiv>
+          <NormalInputContainerDiv>
+            <NormalInputTitleDiv>부양자분의 연령대</NormalInputTitleDiv>
+            <NormalInputContentDiv
+              interface="popover"
+              placeholder="연령대를 선택해주세요(선택)"
+            >
+              <NormalCategoryInput value="20대">20대</NormalCategoryInput>
+              <NormalCategoryInput value="30대">30대</NormalCategoryInput>
+              <NormalCategoryInput value="40대">40대</NormalCategoryInput>
+              <NormalCategoryInput value="50대 이상">
+                50대 이상
+              </NormalCategoryInput>
+            </NormalInputContentDiv>
+          </NormalInputContainerDiv>
+          <NormalInputContainerDiv>
+            <NormalInputTitleDiv>
+              메일 주소<IonText color="danger">*</IonText>
+            </NormalInputTitleDiv>
+            <NormalIonItem01 $shouldShake={shouldShake}>
+              <NormalTextInput01
+                type="text"
+                value={email}
+                onIonInput={(e) => {
+                  setEmail(e.detail.value as string);
+                  if (!isEmail && checkIsEmail(email)) {
+                    setIsEmail(true);
+                  }
+                }}
+                placeholder="서비스 론칭 시 알림 받을 메일 주소를 입력해주세요"
+              ></NormalTextInput01>
+            </NormalIonItem01>
+          </NormalInputContainerDiv>
           {isEmail ? (
             <></>
           ) : (
             <StyledAlertText>올바른 이메일 형식이 아닙니다.</StyledAlertText>
           )}
-
+          <NormalInputContainerDiv>
+            <NormalInputTitleDiv>서비스 신청 이유</NormalInputTitleDiv>
+            <NormalIonItem02>
+              <NormalTextInput02
+                labelPlacement="floating"
+                value={content02}
+                onIonInput={(e) => {
+                  setContent02(e.detail.value as string);
+                }}
+                placeholder="서비스 신청 이유에 대해 자유로이 기술해주세요"
+              >
+                <div slot="label">
+                  Comments <IonText color="medium">(선택)</IonText>
+                </div>
+              </NormalTextInput02>
+            </NormalIonItem02>
+          </NormalInputContainerDiv>
+          <NormalInputContainerDiv>
+            <NormalInputTitleDiv>
+              론칭될 서비스에 기대하는 점
+            </NormalInputTitleDiv>
+            <NormalIonItem03>
+              <NormalTextInput03
+                labelPlacement="floating"
+                value={content03}
+                onIonInput={(e) => {
+                  setContent03(e.detail.value as string);
+                }}
+                placeholder="론칭될 매칭서비스 온라인 플랫폼 ‘매치’ 서비스에 기대하는 점을 자유롭게 기술해주세요"
+              >
+                <div slot="label">
+                  Comments <IonText color="medium">(선택)</IonText>
+                </div>
+              </NormalTextInput03>
+            </NormalIonItem03>
+          </NormalInputContainerDiv>
           <BtnBox>
-            <StyledIonButton type="submit">제출</StyledIonButton>
-            <StyledIonButton
-              onClick={() => {
-                dismiss();
-                setTimeout(() => initState(), 500);
-              }}
-            >
-              취소
-            </StyledIonButton>
+            <StyledIonButton type="submit">제출하기</StyledIonButton>
           </BtnBox>
-        </form>
+        </FormTextDiv>
       </BaseDiv>
     </StyledIonModal>
   );
@@ -111,16 +176,21 @@ const BaseDiv = styled.div`
   justify-content: center;
   align-items: center;
 
-  /* height: 12rem; */
-  width: 16rem;
+  height: 36rem;
+  width: 20rem;
 
   border-radius: 16px;
+  background-color: #e6efff;
 `;
 
-const FormTextDiv = styled.div`
-  width: 13rem;
+const FormTextDiv = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 85%;
   text-align: left;
-  font-size: 0.8rem;
   color: var(--ion-color-medium-shade);
 `;
 
@@ -131,15 +201,72 @@ const shakeAnimation = keyframes`
   75% { transform: translateX(-5px); }
 `;
 
-const StyledAlertText = styled.div`
-  width: 13rem;
-  text-align: right;
-  font-size: 0.5rem;
-  color: var(--ion-color-danger);
+const BtnBox = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 17rem;
 `;
 
-const StyledIonInputBox = styled.div<{ $shouldShake: boolean }>`
-  margin-top: 1rem;
+const StyledIonButton = styled(IonButton)`
+  width: 100%;
+  --border-radius: 1rem;
+
+  --background-color: #71c1ee;
+`;
+
+const NormalInputContainerDiv = styled.div`
+  margin-top: 0.5rem;
+  /* margin-bottom: 1rem; */
+  width: 100%;
+
+  color: #3c3c3c;
+`;
+
+const NormalInputTitleDiv = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+
+  height: 1.5rem;
+  width: 100%;
+
+  font-size: 1rem;
+`;
+
+const NormalInputContentDiv = styled(IonSelect)`
+  padding: 0.5rem 0.5rem;
+
+  display: flex;
+  justify-content: start;
+  align-items: center;
+
+  height: 2.25rem;
+  width: 100%;
+
+  background-color: white;
+  border: 1px solid #c8c8c8;
+  border-radius: 0.3rem;
+`;
+
+const NormalCategoryInput = styled(IonSelectOption)`
+  padding: 0.5rem 0.5rem;
+  width: 100%;
+
+  font-size: 1.1rem;
+`;
+
+const NormalIonItem01 = styled(IonItem)<{ $shouldShake: boolean }>`
+  height: 3rem;
+  width: 100%;
+
+  background-color: white;
+  border: 1px solid #c8c8c8;
+  border-radius: 0.3rem;
+
   ${(props) =>
     props?.$shouldShake &&
     css`
@@ -147,21 +274,56 @@ const StyledIonInputBox = styled.div<{ $shouldShake: boolean }>`
     `}
 `;
 
-const StyledIonInput = styled(IonInput)`
-  width: 13rem;
-  border-radius: 1rem;
-  font-size: 0.9rem;
-`;
-
-const BtnBox = styled.div`
-  margin-top: 1rem;
-
+const NormalTextInput01 = styled(IonInput)`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  width: 13rem;
+
+  height: 3rem;
+  width: 100%;
+
+  font-size: 0.7rem;
 `;
 
-const StyledIonButton = styled(IonButton)`
-  --border-radius: 1rem;
+const StyledAlertText = styled.div`
+  width: 100%;
+  text-align: right;
+  font-size: 0.5rem;
+  color: var(--ion-color-danger);
+`;
+
+const NormalIonItem02 = styled(IonItem)`
+  height: 5rem;
+  width: 100%;
+
+  background-color: white;
+  border: 1px solid #c8c8c8;
+  border-radius: 0.3rem;
+`;
+
+const NormalTextInput02 = styled(IonTextarea)`
+  margin: 0;
+
+  height: 5rem;
+  width: 100%;
+  overflow-y: auto;
+
+  font-size: 0.7rem;
+`;
+
+const NormalIonItem03 = styled(IonItem)`
+  height: 5rem;
+  width: 100%;
+
+  background-color: white;
+  border: 1px solid #c8c8c8;
+  border-radius: 0.3rem;
+`;
+
+const NormalTextInput03 = styled(IonTextarea)`
+  height: 5rem;
+  width: 100%;
+  overflow-y: auto;
+
+  font-size: 0.7rem;
 `;
