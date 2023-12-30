b107c1647895baf1087aa43fc5cc810bc92981cb
khoon9 <kshun08@dongguk.edu>
2023-12-31 00:10:12 +0900

    feat: ga4 초기화 방식 변경

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import { IonApp } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 
 import { setupIonicReact } from "@ionic/react";
+import ReactGA from "react-ga4";
 
 // IonicReact 초기화
 setupIonicReact();
+
+// ga4 초기화
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <IonApp>
